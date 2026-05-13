8fa3a3e78322a3c8f15fb8dc0f2ef36023e4c8de
Adam Hutchison <adam.hutchison@hey.com>
2026-05-12 21:48:56 -0500

    WIP: Remove OhMyZsh and load directly

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -1,7 +1,46 @@
-# Additional bundler aliases
+# General aliases
+alias ...=../..
+alias ....=../../..
+alias .....=../../../..
+alias ......=../../../../..
+alias 1='cd -1'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -4'
+alias 5='cd -5'
+alias 6='cd -6'
+alias 7='cd -7'
+alias 8='cd -8'
+alias 9='cd -9'
+alias cd..='cd ..'
+alias globurl='noglob urlglobber '
+alias l='ls -lah'
+alias la='ls -lAh'
+alias ll='ls -lh'
+alias lp='ls -p'
+alias ls='ls -G'
+alias lsa='ls -lah'
+alias md='mkdir -p'
+alias rd=rmdir
+alias _='sudo '
+
+# Grep aliases
+alias egrep='grep -E'
+alias fgrep='grep -F'
+
+# Bundler aliases
 alias b='bundle install'
+alias ba='bundle add'
 alias bc='bundle console'
+alias bck='bundle check'
+alias bcn='bundle clean'
+alias be='bundle exec'
+alias bi='bundle install'
+alias bl='bundle list'
 alias bo='EDITOR=nova bundle open'
+alias bout='bundle outdated'
+alias bp='bundle package'
+alias bu='bundle update'
 alias bx='bundle exec'
 alias tb='t b'
 alias tbu='t bu'
@@ -26,28 +65,32 @@ alias ghpr='gh pr create --web'
 alias m='mate -w .'
 alias n='nova .'
 
-# Misc
+# Development aliases
 alias bullet='BULLET=1'
-alias cd..='cd ..'
 alias ci='bin/rails ci'
-alias cloud='cd ~/Library/Mobile\ Documents/com~apple~CloudDocs'
 alias derp='RUBYOPT=W0'
-alias dotfiles='cd ~/.dotfiles'
-alias http='python -m SimpleHTTPServer'
 alias jitless='JRUBY_OPTS="$JRUBY_OPTS -J-Djruby.compile.mode=OFF"'
 alias jrbdev='JRUBY_OPTS="$JRUBY_OPTS --dev --disable:did_you_mean --debug"'
-alias kp='ps auxwww' # the "kp" alias ("que pasa"), in honor of tony p.
-alias lp='ls -p'
-alias mspec='rspec $(git ls-files --modified spec)'
-alias ns='nslookup'
-alias o='open .'
-alias path='echo $PATH'
 alias redis='redis-server $HOMEBREW_REPOSITORY/etc/redis.conf'
-alias reload='. ~/.zshrc'
-alias spf='t bin/rspec --only-failures'
+alias spec='t rspec'
+alias specf='spec --only-failures'
+alias specm='spec $(git ls-files --modified spec)'
+alias sp='spec'
+alias spf='specf'
+alias spm='specm'
 alias std='standardize'
 alias std!='standardize!'
 alias standardize='bin/standardrb --fix'
 alias standardize!='bin/standardrb --fix-unsafely'
 alias t='turbo'
 alias turbo='derp jrbdev'
+
+# Misc aliases
+alias cloud='cd ~/Library/Mobile\ Documents/com~apple~CloudDocs'
+alias dotfiles='cd ~/.dotfiles'
+alias http='python -m SimpleHTTPServer'
+alias kp='ps auxwww' # the "kp" alias ("que pasa"), in honor of tony p.
+alias ns='nslookup'
+alias o='open .'
+alias path='echo $PATH'
+alias reload='. ~/.zshrc'

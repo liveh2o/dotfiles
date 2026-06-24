3195a68cd3bfa005e732c58d050a9a8a0fe12e51
Adam Hutchison <adam.hutchison@pairteam.com>
2026-06-23 19:00:28 -0500

    Fix Fish path collision
    The path function collides with a built-in Fish function and is causing
    issues. Rename it to paths to avoid this.

--- config/fish/functions/path.fish
+++
@@ -1,5 +0,0 @@
-function path -d "Print PATH entries one per line"
-    for p in $PATH
-        echo $p
-    end
-end
---
+++ config/fish/functions/paths.fish
@@ -0,0 +1,5 @@
+function paths -d "Print PATH entries one per line"
+    for p in $PATH
+        echo $p
+    end
+end

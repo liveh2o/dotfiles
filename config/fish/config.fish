9f34bb5babb01f933ed7ea5fbef3c908fb16647f
Adam Hutchison <adam.hutchison@hey.com>
2026-07-01 16:00:05 -0500

    Accept Fish autocompletion with Tab
    Update the Fish config to accept autocompeletion with Tab along with
    Right Arrow. Also add some annotations to keep things clear.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,12 +6,18 @@ set -gx HOMEBREW_BUNDLE_FILE $DOTFILES/Brewfile
 set -gx JRUBY_OPTS "--dev --disable=did_you_mean"
 set -gx JRUBY_LATEST_VERSION 10.1
 
+# Add ~/.bin and ~/.local/bin to $PATH if they exist
 for bin_path in $HOME/.bin $HOME/.local/bin
     if test -d $bin_path
         fish_add_path $bin_path
     end
 end
 
+# Suppress the fish startup message
 set -g fish_greeting
 
-set -g fish_glob_sort_as_case false
+# Interactive-only settings...
+if status is-interactive
+    # Accept autocompletion with Tab instead of Right Arrow
+    bind \t accept-autosuggestion
+end
